7cda40e2810c793639f55424d4e59d25e695a62d
radionnazmiev <radionnazmiev@gmail.com>
2021-07-31 13:40:01 -0700

    1:39

--- pages/components/Loading.js
+++ pages/components/Loading.js
@@ -1,11 +1,14 @@
 import { Circle } from "better-react-spinkit";
+import Image from "next/image";
+
 function Loading() {
   return (
     <center
       style={{ display: "grid", placeItems: "center center", height: "100vh" }}
     >
       <div>
-        <img
+        <Image
+          width={200}
           height={200}
           style={{ marginBottom: 10 }}
           src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
